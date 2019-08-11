cc3735e7d20142887ae46065c655d71bf772bbba
Lukie Kang <lukiek+f@gmail.com>
2019-08-11 19:56:29 +0000

    first commit after making gem

---
+++ f1_2019_competitors/lib/f1_2019_competitors.rb
@@ -0,0 +1,6 @@
+require "f1_2019_competitors/version"
+
+module F12019Competitors
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ f1_2019_competitors/lib/f1_2019_competitors/version.rb
@@ -0,0 +1,3 @@
+module F12019Competitors
+  VERSION = "0.1.0"
+end
